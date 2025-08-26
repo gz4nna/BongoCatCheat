fd930e2e8e89e7500bb074c9ecfbef23b27f59cf
GZ4nna <gz4nna@gmail.com>
2025-08-27 01:18:30 +0800

    第一版
    第一版,好像跑着挺顺利?速度还行,再快就会识别漏了

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace BongoCatCheat
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Input;
+using FlaUI.UIA3;
+
+namespace BongoCatCheat;
+
+public class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        System.Diagnostics.Process.Start("notepad.exe");
+        var app = FlaUI.Core.Application.Attach("notepad");
+        using var automation = new UIA3Automation();
+        var window = app.GetMainWindow(automation);
+        var _conditionFactory = new ConditionFactory(new UIA3PropertyLibrary());
+        var textBox = window.FindFirstDescendant(_conditionFactory.ByClassName("Edit")).AsTextBox();
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Keyboard.Press(FlaUI.Core.WindowsAPI.VirtualKeyShort.KEY_A);
+            await Task.Delay(20);
+            Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.KEY_A);
+            await Task.Delay(20);
+
+            Keyboard.Press(FlaUI.Core.WindowsAPI.VirtualKeyShort.BACK);
+            await Task.Delay(20);
+            Keyboard.Release(FlaUI.Core.WindowsAPI.VirtualKeyShort.BACK);
+            await Task.Delay(20);
         }
     }
 }
